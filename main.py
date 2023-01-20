4c00f0cd8e763fe5a2a718d8dc083a799039b13b
rajivranjanmars <rajivranjanmars@gmail.com>
2023-01-21 03:17:20 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import qrcode
+
+def generate_qr_code(link):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+
+    qr.add_data(link)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save('qr.png')
+
+link =input("Enter the link: ")
+generate_qr_code(link)
